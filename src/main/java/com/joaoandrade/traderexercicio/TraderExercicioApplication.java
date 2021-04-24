94b55519fa89d83561c446e681c90ad06db1e3b8
joaoandradejava <andradepinheironeto24@hotmail.com>
2021-04-24 02:38:43 -0300

    Primeiro Commit

---
+++ src/main/java/com/joaoandrade/traderexercicio/TraderExercicioApplication.java
@@ -0,0 +1,13 @@
+package com.joaoandrade.traderexercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderExercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderExercicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaoandrade/traderexercicio/TraderExercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoandrade.traderexercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraderExercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
